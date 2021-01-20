d514b41a37e8d00930c4fb2661204af2d878d303
Devl326789 <Devl987623@gmail.com>
2021-01-19 18:38:26 -0800

    first commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('this works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/l28Y9CR12OZbUKnrH33G/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
+      ),
+    );
+  }
+}
