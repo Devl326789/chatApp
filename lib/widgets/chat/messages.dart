903ee3e1718ca6faa5fe4ea5dce7181f4ff3ccd9
Devl326789 <Devl987623@gmail.com>
2021-01-22 19:38:27 -0800

    Improving chat bubbles, adding document key

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../chat/message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (ctx, chastSnapshot) {
-        if (chastSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final chatDocs = chastSnapshot.data.documents;
-        return ListView.builder(
-          reverse: true,
-          itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text']),
-          itemCount: chatDocs.length,
+        return StreamBuilder(
+          stream: Firestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (ctx, chastSnapshot) {
+            if (chastSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final chatDocs = chastSnapshot.data.documents;
+            return ListView.builder(
+              reverse: true,
+              itemBuilder: (ctx, index) => MessageBubble(
+                chatDocs[index]['text'],
+                chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                key: ValueKey(chatDocs[index].documentID),
+              ),
+              itemCount: chatDocs.length,
+            );
+          },
         );
       },
     );
